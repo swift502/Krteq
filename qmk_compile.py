01fa34d0179b3f92d7d6e7d207b138b30aec12ad
Jan Bláha <blaha.j502@gmail.com>
2026-02-14 00:53:36 +0100

    cleanup

--- qmk_compile.py
+++ qmk_compile.py
@@ -16,40 +16,33 @@ root_remote = Path.home() / "qmk_firmware"
 kb_remote = root_remote / "keyboards" / "krteq"
 bin_remote = root_remote / "krteq_default.uf2"
 
-def copy_qmk_folder():
-    # Remove existing
-    if kb_remote.exists():
-        shutil.rmtree(kb_remote)
-        print(f"Removed existing folder '{kb_remote}'.")
-
-    # Setup qmk keyboard folder
-    shutil.copytree(kb_local, kb_remote)
-    print(f"Copied '{kb_local}' to '{kb_remote}'.")
-
-def run_qmk_compile():
-    # Environment
-    # https://docs.qmk.fm/other_vscode#msys2-setup
-    env = os.environ.copy()
-    env["MSYSTEM"] = "MINGW64"
-
-    # Args
-    command = "qmk compile -kb krteq -km default --clean"
-    args = [msys_exe, "--login", "-c", command]
-
-    # Run
-    print()
-    subprocess.run(args, env=env, check=True)
-    print()
-
-def retrieve_bin():
-    # Move .uf2 file
-    shutil.move(bin_remote, bin_local)
-    print(f"Moved '{bin_remote}' to '{bin_local}'.")
-
-    # Clean up
+# Remove existing
+if kb_remote.exists():
     shutil.rmtree(kb_remote)
-    print(f"Cleaned up '{kb_remote}'.")
+    print(f"Removed existing folder '{kb_remote}'.")
 
-copy_qmk_folder()
-run_qmk_compile()
-retrieve_bin()
+# Setup qmk keyboard folder
+shutil.copytree(kb_local, kb_remote)
+print(f"Copied '{kb_local}' to '{kb_remote}'.")
+
+# Environment
+# https://docs.qmk.fm/other_vscode#msys2-setup
+env = os.environ.copy()
+env["MSYSTEM"] = "MINGW64"
+
+# Args
+command = "qmk compile -kb krteq -km default --clean"
+args = [msys_exe, "--login", "-c", command]
+
+# Run
+print()
+subprocess.run(args, env=env, check=True)
+print()
+
+# Move .uf2 file
+shutil.move(bin_remote, bin_local)
+print(f"Moved '{bin_remote}' to '{bin_local}'.")
+
+# Clean up
+shutil.rmtree(kb_remote)
+print(f"Cleaned up '{kb_remote}'.")
