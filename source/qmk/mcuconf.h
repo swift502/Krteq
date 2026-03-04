1cfa7db16a6fe8957a68f461b068093c260c25d8
Jan Bláha <blaha.j502@gmail.com>
2026-03-04 13:37:56 +0100

    led intensity

--- source/qmk/mcuconf.h
+++ source/qmk/mcuconf.h
@@ -2,11 +2,11 @@
 
 #include_next <mcuconf.h>
 
-// #undef RP_PWM_USE_PWM0
-// #define RP_PWM_USE_PWM0 TRUE
+#undef RP_PWM_USE_PWM0
+#define RP_PWM_USE_PWM0 TRUE
 
-// #undef RP_PWM_USE_PWM1
-// #define RP_PWM_USE_PWM1 TRUE
+#undef RP_PWM_USE_PWM1
+#define RP_PWM_USE_PWM1 TRUE
 
 #undef RP_PWM_USE_PWM6
 #define RP_PWM_USE_PWM6 TRUE
