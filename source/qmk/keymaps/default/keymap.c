34d76cdaeb773d2a90bc0902aac3a827c81660d1
Jan Bláha <blaha.j502@gmail.com>
2026-08-22 15:00:48 +0200

    qmk update

--- source/qmk/keymaps/default/keymap.c
+++ source/qmk/keymaps/default/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KRT_VOL, KC_LALT, MO(1),        KC_SPC,      MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     [1] = LAYOUT_default(
-        KC_MPLY,                                                                                           KC_NUM,
+        TG(3),                                                                                             KC_NUM,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
         KC_INS,  _______, KC_UP,   _______, KC_PGUP, KC_P7,   KC_P8,   KC_P9,   _______, _______, _______, _______,
         KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_P4,   KC_P5,   KC_P6,   _______, KC_LBRC, KC_RBRC, KC_BSLS,
@@ -36,11 +36,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______,      KC_P0,       KC_PDOT, KC_HOME, _______, _______, KC_END
     ),
     [2] = LAYOUT_default(
-        BL_BRTG,                                                                                           BL_TOGG,
+        _______,                                                                                           RM_TOGG,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU,
-        _______, _______, BL_UP,   _______, _______, _______, _______, _______, _______, KC_PSLS, KC_PAST, KC_PMNS,
-        _______, _______, BL_DOWN, _______, _______, _______, _______, _______, _______, _______, _______, KC_PPLS,
+        RM_NEXT, RM_HUED, RM_VALU, RM_HUEU, RM_SPDU, _______, _______, _______, _______, KC_PSLS, KC_PAST, KC_PMNS,
+        RM_PREV, RM_SATD, RM_VALD, RM_SATU, RM_SPDD, _______, _______, _______, _______, _______, _______, KC_PPLS,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RSFT(KC_ENT),
         _______, _______, _______, _______, _______,      _______,     _______, _______, _______, _______, RCTL(KC_ENT)
+    ),
+    [3] = LAYOUT_default(
+        XXXXXXX,                                                                                           XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MO(4),        XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+    [4] = LAYOUT_default(
+        TG(3),                                                                                             _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______,      _______,     _______, _______, _______, _______, _______
     )
 };
--- source/qmk/krteq.c
+++ source/qmk/krteq.c
@@ -1,28 +1,4 @@
 #include QMK_KEYBOARD_H
-#include "hal.h"
-
-// PWM0 A - GP0 (Num Lock)
-// PWM0 B - GP1 (Caps Lock)
-static PWMConfig pwm0_cfg = {
-    .frequency = 1000000,
-    .period = 511,
-    .callback = NULL,
-    .channels = {
-        {PWM_OUTPUT_ACTIVE_HIGH, NULL},
-        {PWM_OUTPUT_ACTIVE_HIGH, NULL}
-    }
-};
-
-// PWM1 A - GP2 (Scroll Lock)
-static PWMConfig pwm1_cfg = {
-    .frequency = 1000000,
-    .period = 511,        
-    .callback = NULL,
-    .channels = {
-        {PWM_OUTPUT_ACTIVE_HIGH, NULL},
-        {PWM_OUTPUT_DISABLED, NULL}
-    }
-};
 
 bool process_record_kb(uint16_t keycode, keyrecord_t *record)
 {
@@ -56,7 +32,7 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record)
         case KC_B:
             if (record->event.pressed && double_shift)
             {
-                reset_keyboard();
+                tap_code16(QK_BOOTLOADER);
                 return false;
             }
             break;
@@ -64,8 +40,7 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record)
         case KC_C:
             if (record->event.pressed && double_shift)
             {
-                eeconfig_disable();
-                soft_reset_keyboard();
+                tap_code16(QK_CLEAR_EEPROM);
                 return false;
             }
             break;
@@ -74,33 +49,95 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record)
     return process_record_user(keycode, record);
 }
 
-void keyboard_post_init_kb(void)
+#ifdef OLED_ENABLE
+bool oled_task_kb(void)
 {
-    // 2.19.2. Function Select (PWM)
-    // https://pip-assets.raspberrypi.com/categories/814-rp2040/documents/RP-008371-DS-1-rp2040-datasheet.pdf?disposition=inline
-    palSetLineMode(GP0, PAL_MODE_ALTERNATE(4));
-    palSetLineMode(GP1, PAL_MODE_ALTERNATE(4));
-    palSetLineMode(GP2, PAL_MODE_ALTERNATE(4));
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("ACCENT ") : PSTR("    "), false);
 
-    pwmStart(&PWMD0, &pwm0_cfg);
-    pwmStart(&PWMD1, &pwm1_cfg);
-
-    // Initialize off
-    pwmEnableChannel(&PWMD0, 0, 0);
-    pwmEnableChannel(&PWMD0, 1, 0);
-    pwmEnableChannel(&PWMD1, 0, 0);
-}
-
-bool led_update_kb(led_t led_state)
-{
-    bool res = led_update_user(led_state);
-
-    if (res)
+    if (get_highest_layer(layer_state) >= 3)
     {
-        pwmEnableChannel(&PWMD0, 0, led_state.num_lock ? 1 : 0);
-        pwmEnableChannel(&PWMD0, 1, led_state.caps_lock ? 1 : 0);
-        pwmEnableChannel(&PWMD1, 0, led_state.scroll_lock ? 1 : 0);
+        oled_write_P(led_state.scroll_lock ? PSTR("INPUT LOCK") : PSTR("    "), false);
     }
-
-    return res;
+    
+    return oled_task_user();
 }
+#endif
+
+const is31fl3733_led_t PROGMEM g_is31fl3733_leds[IS31FL3733_LED_COUNT] = {
+    //  R          G          B
+    // Row 0
+    {0, SW1_CS1,   SW2_CS1,   SW3_CS1},
+    {0, SW10_CS1,  SW11_CS1,  SW12_CS1},
+
+    // Row 1
+    {0, SW1_CS2,   SW2_CS2,   SW3_CS2},
+    {0, SW4_CS2,   SW5_CS2,   SW6_CS2},
+    {0, SW7_CS2,   SW8_CS2,   SW9_CS2},
+    {0, SW10_CS2,  SW11_CS2,  SW12_CS2},
+    {0, SW1_CS3,   SW2_CS3,   SW3_CS3},
+    {0, SW4_CS3,   SW5_CS3,   SW6_CS3},
+    {0, SW7_CS3,   SW8_CS3,   SW9_CS3},
+    {0, SW10_CS3,  SW11_CS3,  SW12_CS3},
+    {0, SW1_CS4,   SW2_CS4,   SW3_CS4},
+    {0, SW4_CS4,   SW5_CS4,   SW6_CS4},
+    {0, SW7_CS4,   SW8_CS4,   SW9_CS4},
+    {0, SW10_CS4,  SW11_CS4,  SW12_CS4},
+
+    // Row 2
+    {0, SW1_CS5,   SW2_CS5,   SW3_CS5},
+    {0, SW4_CS5,   SW5_CS5,   SW6_CS5},
+    {0, SW7_CS5,   SW8_CS5,   SW9_CS5},
+    {0, SW10_CS5,  SW11_CS5,  SW12_CS5},
+    {0, SW1_CS6,   SW2_CS6,   SW3_CS6},
+    {0, SW4_CS6,   SW5_CS6,   SW6_CS6},
+    {0, SW7_CS6,   SW8_CS6,   SW9_CS6},
+    {0, SW10_CS6,  SW11_CS6,  SW12_CS6},
+    {0, SW1_CS7,   SW2_CS7,   SW3_CS7},
+    {0, SW4_CS7,   SW5_CS7,   SW6_CS7},
+    {0, SW7_CS7,   SW8_CS7,   SW9_CS7},
+    {0, SW10_CS7,  SW11_CS7,  SW12_CS7},
+
+    // Row 3
+    {0, SW1_CS8,   SW2_CS8,   SW3_CS8},
+    {0, SW4_CS8,   SW5_CS8,   SW6_CS8},
+    {0, SW7_CS8,   SW8_CS8,   SW9_CS8},
+    {0, SW10_CS8,  SW11_CS8,  SW12_CS8},
+    {0, SW1_CS9,   SW2_CS9,   SW3_CS9},
+    {0, SW4_CS9,   SW5_CS9,   SW6_CS9},
+    {0, SW7_CS9,   SW8_CS9,   SW9_CS9},
+    {0, SW10_CS9,  SW11_CS9,  SW12_CS9},
+    {0, SW1_CS10,  SW2_CS10,  SW3_CS10},
+    {0, SW4_CS10,  SW5_CS10,  SW6_CS10},
+    {0, SW7_CS10,  SW8_CS10,  SW9_CS10},
+    {0, SW10_CS10, SW11_CS10, SW12_CS10},
+
+    // Row 4
+    {0, SW1_CS11,  SW2_CS11,  SW3_CS11},
+    {0, SW4_CS11,  SW5_CS11,  SW6_CS11},
+    {0, SW7_CS11,  SW8_CS11,  SW9_CS11},
+    {0, SW10_CS11, SW11_CS11, SW12_CS11},
+    {0, SW1_CS12,  SW2_CS12,  SW3_CS12},
+    {0, SW4_CS12,  SW5_CS12,  SW6_CS12},
+    {0, SW7_CS12,  SW8_CS12,  SW9_CS12},
+    {0, SW10_CS12, SW11_CS12, SW12_CS12},
+    {0, SW1_CS13,  SW2_CS13,  SW3_CS13},
+    {0, SW4_CS13,  SW5_CS13,  SW6_CS13},
+    {0, SW7_CS13,  SW8_CS13,  SW9_CS13},
+    {0, SW10_CS13, SW11_CS13, SW12_CS13},
+
+    // Row 5
+    {0, SW1_CS14,  SW2_CS14,  SW3_CS14},
+    {0, SW4_CS14,  SW5_CS14,  SW6_CS14},
+    {0, SW7_CS14,  SW8_CS14,  SW9_CS14},
+    {0, SW10_CS14, SW11_CS14, SW12_CS14},
+    {0, SW1_CS15,  SW2_CS15,  SW3_CS15},
+    {0, SW4_CS15,  SW5_CS15,  SW6_CS15},
+    {0, SW10_CS15, SW11_CS15, SW12_CS15},
+    {0, SW1_CS16,  SW2_CS16,  SW3_CS16},
+    {0, SW4_CS16,  SW5_CS16,  SW6_CS16},
+    {0, SW7_CS16,  SW8_CS16,  SW9_CS16},
+    {0, SW10_CS16, SW11_CS16, SW12_CS16},
+};
