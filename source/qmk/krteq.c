69f6a186e59cc7c9d8a1aa3d8f511fb80835dfb5
Jan Bláha <blaha.j502@gmail.com>
2026-08-24 22:25:09 +0200

    update

--- source/qmk/krteq.c
+++ source/qmk/krteq.c
@@ -100,14 +100,20 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record)
         case KC_B:
             if (record->event.pressed && double_shift)
             {
-                return process_quantum(QK_BOOTLOADER, record);
+                if (!process_quantum(QK_BOOTLOADER, record))
+                {
+                    return false;
+                }
             }
             break;
 
         case KC_C:
             if (record->event.pressed && double_shift)
             {
-                return process_quantum(QK_CLEAR_EEPROM, record);
+                if (!process_quantum(QK_CLEAR_EEPROM, record))
+                {
+                    return false;
+                }
             }
             break;
     }
